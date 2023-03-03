1d5ddd3484cd8318343a27643690125997e2853a
SoniaBrasa <virtuales@gmx.es>
2023-03-03 01:30:44 +0100

    Cinemática simple

--- Assets/Scripts/EnemyShip/ShipBuoyancyKinematic.cs
+++ Assets/Scripts/EnemyShip/ShipBuoyancyKinematic.cs
@@ -2,6 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ShipBuoyancyKinematic : MonoBehaviour {
+public class ShipBuoyancyKinematic : MonoBehaviour
+{
+    float acceleration;
+    float initialImpulse;
+    float verticalSpeed;
 
+
+    void Start()
+    {
+        acceleration = -1;
+        initialImpulse = 1.5f;
+
+        // Impulso hacia arriba para iniciar el balanceo
+        transform.position += transform.up * initialImpulse;
+    }
+
+    void Update()
+    {
+        float waterLevel = transform.position.y;
+
+        verticalSpeed += acceleration * waterLevel * Time.deltaTime;
+
+        transform.position += transform.up * verticalSpeed * Time.deltaTime;
+    }
 }
